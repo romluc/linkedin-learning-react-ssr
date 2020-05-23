9e78dbc6798b9dec51283052e3f5a5f3c3eadaeb
romluc <romulofilhofilho@gmail.com>
2020-05-23 18:23:07 -0300

    Implement reducers

--- store.js
+++ store.js
@@ -5,14 +5,42 @@ import data from './data/data.json';
 
 // Initial state
 const startState = {
-  card: [],
+  cards: [],
 };
 
 // Actions
 export const initialCards = () => {
-  return {};
+  return {
+    type: 'INITIALCARDS',
+    cards: data,
+  };
 };
 
+export const addItem = (item) => {
+  return {
+    type: 'ADD',
+    item,
+  };
+};
+
+// Reducers
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INITIALCARDS':
+      return {
+        cards: action.cards,
+      };
+    case 'ADD':
+      return {
+        ...state,
+        cards: [...state, action.item],
+      };
+    default:
+      return state;
+  }
+};
+
+// Create store
 export const initStore = (initialState = startState) => {
   return createStore(
     reducer,
